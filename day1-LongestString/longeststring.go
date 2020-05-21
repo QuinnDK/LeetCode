00075f3166cea7599f77d17aa6a7ca271fefd8ac
QuinnDK <18207205276@163.com>
2020-05-21 09:17:53 +0800

    中心向两边扩展

--- day1-LongestString/longeststring.go
+++ day1-LongestString/longeststring.go
@@ -4,36 +4,38 @@ import "fmt"
 
 func main() {
 
-	var s string = "ababd"
+	var s string = "abcdc"
 	longestPalindrome(s)
 }
 func longestPalindrome(s string) string {
-	if len(s)==0{
+	if len(s) == 0 {
 		return ""
 	}
-	max:=0
-	result:=""
-	for i:=range s{
-		len1,str1:=expandAroundCenter(s,i,i)
-		if len1>max{
-			max=len1
-			result=str1
+	max := 0
+	result := ""
+	for i := range s {
+		len1, str1 := expandAroundCenter(s, i, i) //奇数位字符串
+		if len1 > max {
+			max = len1
+			result = str1
 		}
-		len2,str2:=expandAroundCenter(s,i,i+1)
-		if len2>max{
-			max=len2
-			result=str2
+		len2, str2 := expandAroundCenter(s, i, i+1) //偶数位字符串
+		if len2 > max {
+			max = len2
+			result = str2
 		}
 	}
 	fmt.Printf(result)
 	return ""
 }
-func expandAroundCenter( s string, left, right int) (int,string) {
+
+//对输入的字符串做处理，回文从他的中心向两边展开，
+func expandAroundCenter(s string, left, right int) (int, string) {
 	L := left
 	R := right
 	for L >= 0 && R < len(s) && s[L] == s[R] {
 		L--
 		R++
 	}
-	return R - L - 1,s[L+1:R]
+	return R - L - 1, s[L+1 : R] //
 }

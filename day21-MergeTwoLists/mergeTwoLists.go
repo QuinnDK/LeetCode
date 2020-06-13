ccf0268bc6c8046445f1d8cb3afd60a807d6fae6
QuinnDK <18207205276@163.com>
2020-06-13 10:03:03 +0800

    合并两个链表

--- day21-MergeTwoLists/mergeTwoLists.go
+++ day21-MergeTwoLists/mergeTwoLists.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	_ "LeetCode/init"
+	"fmt"
+)
 
 /*
 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
@@ -14,38 +17,34 @@ import "fmt"
 */
 
 func main() {
-	var head = new(ListNode)
-	head.Val = 1
-	var node1 = new(ListNode)
-	node1.Val = 2
+	//var head = new(ListNode)
+	//head.Val = 1
+	//var node1 = new(ListNode)
+	//node1.Val = 2
+	////var node2 = new(ListNode)
+	////node2.Val = 3
+	////var node3 = new(ListNode)
+	////node3.Val = 4
 	//var node2 = new(ListNode)
-	//node2.Val = 3
-	//var node3 = new(ListNode)
-	//node3.Val = 4
-	var node2 = new(ListNode)
-	node2.Val = 5
+	//node2.Val = 5
+	//
+	//head.Next = node1
+	//node1.Next = node2
+	//
+	//var head1 = new(ListNode)
+	//head1.Val = 1
+	//var node11 = new(ListNode)
+	//node11.Val = 3
+	//var node12 = new(ListNode)
+	//node12.Val = 4
+	//head1.Next = node11
+	//node11.Next = node12
+	//
+	//result := mergeTwoLists(head, head1)
+	//
+	//Shownode(result)
+	fmt.Println(mergeTwoLists(newList([]int{1}), newList([]int{})))
 
-	head.Next = node1
-	node1.Next = node2
-
-	var head1 = new(ListNode)
-	head1.Val = 1
-	var node11 = new(ListNode)
-	node11.Val = 3
-	var node12 = new(ListNode)
-	node12.Val = 4
-	head1.Next = node11
-	node11.Next = node12
-
-	result := mergeTwoLists(head, head1)
-
-	Shownode(result)
-
-}
-
-type ListNode struct {
-	Val  int
-	Next *ListNode
 }
 
 func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {

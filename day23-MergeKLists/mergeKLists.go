19c9d92c70e7ae5a850d06d29227b66c74d76178
QuinnDK <18207205276@163.com>
2020-06-13 10:22:27 +0800

    合并K个排序链表

--- day23-MergeKLists/mergeKLists.go
+++ day23-MergeKLists/mergeKLists.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 /*
 合并 k 个排序链表，返回合并后的排序链表。请分析和描述算法的复杂度。
 
@@ -16,7 +18,12 @@ package main
 */
 
 func main() {
-
+	nums := []int{1, 4, 5}
+	nums1 := []int{1, 3, 4}
+	nums2 := []int{2, 6}
+	List := []*ListNode{newList(nums), newList(nums1), newList(nums2)}
+	result := mergeKLists(List)
+	fmt.Println(result)
 }
 
 func mergeKLists(lists []*ListNode) *ListNode {

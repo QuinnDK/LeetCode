408c2ec7138e3ae0f5d9470eee3c18344c3351e7
QuinnDK <18207205276@163.com>
2020-06-15 10:33:10 +0800

    K 个一组翻转链表

---
+++ day25-ReverseKGroup/reverseKGroup.go
@@ -0,0 +1,62 @@
+package main
+
+import "fmt"
+
+/*
+给你一个链表，每 k 个节点一组进行翻转，请你返回翻转后的链表。
+
+k 是一个正整数，它的值小于或等于链表的长度。
+
+如果节点总数不是 k 的整数倍，那么请将最后剩余的节点保持原有顺序。
+
+
+示例：
+
+给你这个链表：1->2->3->4->5
+
+当 k = 2 时，应当返回: 2->1->4->3->5
+
+当 k = 3 时，应当返回: 3->2->1->4->5
+
+*/
+
+func main() {
+	nums := []int{1, 2, 3, 4, 5}
+	head := newList(nums)
+	result := reverseKGroup(head, 3)
+	fmt.Println(result)
+
+}
+func reverseKGroup(head *ListNode, k int) *ListNode {
+	hair := &ListNode{Next: head}
+	pre := hair
+
+	for head != nil {
+		tail := pre
+		for i := 0; i < k; i++ {
+			tail = tail.Next
+			if tail == nil {
+				return hair.Next
+			}
+		}
+		nex := tail.Next
+		head, tail = myReverse(head, tail)
+		pre.Next = head
+		tail.Next = nex
+		pre = tail
+		head = tail.Next
+	}
+	return hair.Next
+}
+
+func myReverse(head, tail *ListNode) (*ListNode, *ListNode) {
+	prev := tail.Next
+	p := head
+	for prev != tail {
+		nex := p.Next
+		p.Next = prev
+		prev = p
+		p = nex
+	}
+	return tail, head
+}

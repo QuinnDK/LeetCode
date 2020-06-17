9c693bf415f5659e5fce1379f0e0d7c66b6981f9
QuinnDK <18207205276@163.com>
2020-06-17 10:08:19 +0800

    移除元素

---
+++ day27-RemoveElement/removeElement.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+/*
+给你一个数组 nums 和一个值 val，你需要 原地 移除所有数值等于 val 的元素，并返回移除后数组的新长度。
+
+不要使用额外的数组空间，你必须仅使用 O(1) 额外空间并 原地 修改输入数组。
+
+元素的顺序可以改变。你不需要考虑数组中超出新长度后面的元素。
+
+
+
+示例 1:
+
+给定 nums = [3,2,2,3], val = 3,
+
+函数应该返回新的长度 2, 并且 nums 中的前两个元素均为 2。
+
+你不需要考虑数组中超出新长度后面的元素。
+
+*/
+func removeElement(nums []int, val int) int {
+	for i := 0; i < len(nums); {
+		if nums[i] == val {
+			nums = append(nums[:i], nums[i+1:]...)
+		} else {
+			i++
+		}
+	}
+	return len(nums)
+}
+
+func main() {
+	num := []int{3, 2, 2, 3}
+	reslut := removeElement(num, 3)
+	fmt.Println(reslut)
+}

56bec959561cb83c769bffeb8f5c9bd1ecdae2a6
QuinnDK <18207205276@163.com>
2020-06-25 09:41:28 +0800

    在排序数组中查找元素的第一个和最后一个位置

--- day34-SearchRange/searchRange.go
+++ day34-SearchRange/searchRange.go
@@ -18,26 +18,6 @@ import "fmt"
 输入: nums = [5,7,7,8,8,10], target = 6
 输出: [-1,-1]
 */
-func searchRange(nums []int, target int) []int {
-	left := -1
-	right := -1
-
-	if len(nums) == 0 || nums[len(nums)-1] < target {
-		return []int{left, right}
-	}
-	for i := 0; i < len(nums); i++ {
-		if nums[i] == target {
-			left = i
-		}
-	}
-
-	for j := len(nums) - 1; j >= 0; j-- {
-		if nums[j] == target {
-			right = j
-		}
-	}
-	return []int{right, left}
-}
 
 func main() {
 	nums := []int{5, 7, 7, 8, 8, 10}

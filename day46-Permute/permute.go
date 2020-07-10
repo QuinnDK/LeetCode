9b1fe41c6bfb25506368b2dcf95df2e0bd3198b3
QuinnDK <18207205276@163.com>
2020-07-10 09:47:18 +0800

    全排列

--- day46-Permute/permute.go
+++ day46-Permute/permute.go
@@ -45,3 +45,33 @@ func permute(nums []int) [][]int {
 	}
 	return res
 }
+
+//回溯
+func permuteUnique(nums []int) [][]int {
+	res := [][]int{}
+	dfs(nums, &res, 0)
+	return res
+}
+
+func dfs(nums []int, res *[][]int, index int) {
+	if index == len(nums) {
+		*res = append(*res, dump(nums))
+	}
+
+	//m := map[int]int{}
+	for i := index; i < len(nums); i++ {
+		//if _, ok := m[nums[i]]; ok {
+		//	continue
+		//}
+		nums[i], nums[index] = nums[index], nums[i]
+		dfs(nums, res, index+1)
+		nums[i], nums[index] = nums[index], nums[i]
+		//m[nums[i]] = 1
+	}
+}
+
+func dump(a []int) []int {
+	b := make([]int, len(a))
+	copy(b, a)
+	return b
+}

822df5916b6a5112042c9968cf2954eefd8186be
QuinnDK <18207205276@163.com>
2020-08-06 09:15:43 +0800

    最后一个单词的长度

---
+++ day57-LengthOfLastWord/lengthOfLastWord.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+/*
+给定一个仅包含大小写字母和空格 ' ' 的字符串 s，返回其最后一个单词的长度。如果字符串从左向右滚动显示，那么最后一个单词就是最后出现的单词。
+
+如果不存在最后一个单词，请返回 0 。
+
+说明：一个单词是指仅由字母组成、不包含任何空格字符的 最大子字符串。
+
+
+
+示例:
+
+输入: "Hello World"
+输出: 5
+*/
+func lengthOfLastWord(s string) int {
+	sli := strings.Split(s, " ")
+	for i := len(sli) - 1; i >= 0; i-- {
+		if len(sli[i]) != 0 {
+			return len(sli[i])
+		}
+	}
+	return 0
+}
+func main() {
+	str := "Hello World"
+	res := lengthOfLastWord(str)
+	fmt.Println(res)
+}

e9201b92b9ddfbf8be819ae2c0dbe7be2b812046
QuinnDK <18207205276@163.com>
2020-08-19 10:39:34 +0800

    x 的平方根

---
+++ day66-MySqrt/mySqrt.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+/*
+实现 int sqrt(int x) 函数。
+
+计算并返回 x 的平方根，其中 x 是非负整数。
+
+由于返回类型是整数，结果只保留整数的部分，小数部分将被舍去。
+
+示例 1:
+
+输入: 4
+输出: 2
+示例 2:
+
+输入: 8
+输出: 2
+说明: 8 的平方根是 2.82842...,
+     由于返回类型是整数，小数部分将被舍去。
+*/
+func mySqrt(x int) int {
+	l, r := 0, x
+	for l < r {
+		mid := (l + r + 1) >> 1
+		if mid <= x/mid {
+			l = mid
+		} else {
+			r = mid - 1
+		}
+	}
+	return l // r
+}
+func main() {
+	fmt.Println(mySqrt(8))
+}
